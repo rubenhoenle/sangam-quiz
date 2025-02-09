5ea266aeb89d2de5a27c5cd053dcc84e36be9bbb
Ruben Hoenle <git@hoenle.xyz>
2025-02-09 21:35:32 +0100

    chore(nix): add formatter

--- flake.nix
+++ flake.nix
@@ -4,16 +4,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixpkgs-fmt.enable = true;
+            gofmt.enable = true;
+          };
+        };
       in
       {
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatter = treefmtEval.config.build.check self;
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             go
